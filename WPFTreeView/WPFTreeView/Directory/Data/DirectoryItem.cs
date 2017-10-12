48173fc88a58f3e7fdbf6ed90f4b72e4a96a7972
unknown <changchun.huang@gmail.com>
2017-10-12 08:08:08 +0800

    构造ViewModel

---
+++ WPFTreeView/WPFTreeView/Directory/Data/DirectoryItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTreeView
+{
+    public class DirectoryItem
+    {
+        public DirectoryType DirectoryType { get; set; }
+
+        public bool isExpanded { get; set; }
+
+
+    }
+
+    public enum DirectoryType
+    {
+        Drive,
+        Folder,
+        File
+    }
+}
---
+++ WPFTreeView/WPFTreeView/Directory/DirectoryStructure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTreeView
+{
+    /// <summary>
+    /// A Help class for getting directory info
+    /// </summary>
+    public class DirectoryStructure
+    {
+    }
+}
