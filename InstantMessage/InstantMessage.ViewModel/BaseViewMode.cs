62bfe5fc856487e4085285dc46d83963d930bb66
edmundhuang <changchun.huang@gmail.com>
2017-10-26 08:19:47 +0800

    加入字体及样式文件

---
+++ InstantMessage/InstantMessage.ViewModel/BaseViewMode.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace InstantMessage.ViewModel
+{
+    /// <summary>
+    /// 实现 INotifyPropertyChanged 的基本 ViewModel 类
+    /// </summary>
+    public class BaseViewMode : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 属性变化事件
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
